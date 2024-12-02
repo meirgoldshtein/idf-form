7ebaa1a668697763ad33260bdc5885158ae43d75
meirgoldshtein <m0548491057@gmail.com>
2024-12-02 12:39:54 +0200

    added titles & styling

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -2,6 +2,10 @@ import becground from '../../images/tank.jpg'
 import './Body.css'
 export default function Body() {
   return (
-    <div className="image-container"><img src={becground}alt="" /></div>
+    <div className="image-container">
+      <img src={becground}alt="" />
+      <h1 className='title1'>טופס העדפות שירות צבאי</h1>
+      <h2 className='title2'>מלא את העדפותיך לשירות הצבאי</h2>
+    </div>
   )
 }
